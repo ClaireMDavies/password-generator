9c9f6e6ca0c25a8570b2b09fef72bf225c1c4187
Claire Davies <claire.davies@junitech.com>
2020-12-20 16:22:41 +0000

    addition of random maths logic and application to password array

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //Creating arrays for password generation
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "\\", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~", " "];
-var numericalCharacter = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowercaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "\\", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~", " "];
+const numericalCharacter = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const lowercaseCharacter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
 function generatePassword()
@@ -42,15 +42,12 @@ function askForPasswordLength() {
         passwordLength = parseInt(response);
     }
 
-    alert("your password length is " + passwordLength + " characters long");
+    console.log("your password length is " + passwordLength + " characters long");
 
     return passwordLength;
 }
 
 //create a function to select characters
-
-
-
 function characterSelection() {
 
     var passwordCharacters = [];
@@ -67,46 +64,68 @@ function characterSelection() {
         if (specialCharacterSelection == true) {
             anyCharacterTypeSelected = true; 
             passwordCharacters = passwordCharacters.concat(specialCharacters);
-            alert ("your password will contain special characters");
+            console.log("your password will contain special characters");
         }
 
         if (numericalCharacterSelection == true) {
             anyCharacterTypeSelected = true;
             passwordCharacters = passwordCharacters.concat(numericalCharacter);
-            alert("your password will contain numbers");
+            console.log("your password will contain numbers");
         }
 
         if (lowercaseCharacterSelection == true) {
             anyCharacterTypeSelected = true;
             passwordCharacters = passwordCharacters.concat(lowercaseCharacter);
-            alert ("your password will contain lowercase letters");
+            console.log("your password will contain lowercase letters");
         }
     
         if (uppercaseCharacterSelection == true) {
             anyCharacterTypeSelected = true;
             passwordCharacters = passwordCharacters.concat(uppercaseCharacter);
-            alert ("your password will contain capital letters");
+            console.log("your password will contain capital letters");
         }
 
         if (anyCharacterTypeSelected == false)
         {
-            alert("No character types were selected.");
+            alert("No character types were selected, please start again");
         }
     }
-
+    console.log(passwordCharacters);
     return passwordCharacters; 
 }
    
+//creating the function to control the operation of the password generation by password length
+function createPassword(passwordLength, passwordCharacters) {
+    var password = "";
+    while (passwordLength > 0) {
+        password = password + getRandomArrayElement(passwordCharacters);
+        passwordLength --;
+
+    }
+    return password;
+}
+
+// application of the maths logic to the array   
+function getRandomArrayElement(array) {
+    var randomIndex = getRandomInteger(0, array.length);
+    return array[randomIndex]
 
 
-        //    var passwordCharacter = passwordCharacter.concat(specialCharacter);
-        //else 
-        //}
-        //if (numericalCharacterSelection == true) {    
-            //alert ("your password will contain numbers");
-            //var passwordCharacter = passwordCharacter.concat(numericalCharacter);
-       // }
-//}
+}  
+
+//creating the random maths logic
+function getRandomInteger(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+
+}
+    
+    
+
+
+
 
 
 
